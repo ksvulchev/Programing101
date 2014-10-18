5749fe072276d84b806676e0c21a83eff592c223
ecp <ksvulchev@abv.bg>
2014-10-18 10:10:30 +0300

    generate_numbers fix

--- lecture3/generate_numbers.py
+++ lecture3/generate_numbers.py
@@ -2,12 +2,12 @@ import sys
 from random import randint
 
 def main():
-
-		file = open(sys.argv[1], "w") 
-		for i in range(int(sys.argv[2])):
-			contents = str(randint(1, 1000))
-			file.write(contents + " ")
-		file.close()
+	contents = []
+	file = open(sys.argv[1], "w") 
+	for i in range(int(sys.argv[2])):
+		contents.append(str(randint(1, 1000)))
+	file.write(" ".join(contents))
+	file.close()
 
 if __name__ == '__main__':
-	main()
+	main()	
