99e6b7f55dac7384d68d05951997c509cca64661
ecp <ksvulchev@abv.bg>
2014-10-18 10:24:53 +0300

    task29 fix

--- lecture2/task29.py
+++ lecture2/task29.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python
+def groupby(func, seq):
+
+	dictonary = {}
+	for i in seq:
+		key = func(i)
+
+		if key in dictonary:
+			dictonary[key].append(i)
+		else:
+			dictonary[key] = [i]
+	return dictonary
+
+print(groupby(lambda x: x % 3, [0, 1, 2, 3, 4, 5, 6, 7]))
+
