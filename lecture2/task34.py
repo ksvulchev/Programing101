5a767a450fae1fc6c1295c27c081836f25d9088f
ecp <ksvulchev@abv.bg>
2014-10-19 15:30:57 +0300

    Add task34

--- lecture2/task34.py
+++ lecture2/task34.py
@@ -1,7 +1,14 @@
-#!/usr/bin/python	
 from fractions import Fraction
 
 def sort_fractions(fractions):
 
-	return (fractions)
-print (sort_fractions([(2, 3), (1, 2)]))
+	for i in range(len(fractions)):	# malko Bubble sort :D
+		for y in range(1,len(fractions)-i): 
+			if fractions[y-1][0] / fractions[y-1][1] > fractions[y][0] / fractions[y][1]:
+
+
+				fractions[y-1],fractions[y] = fractions[y], fractions[y-1]
+
+
+	return fractions
+print (sort_fractions([(2, 3), (1, 2), (1, 3)]))
